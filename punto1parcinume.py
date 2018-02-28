85653990a3d4f927ab12fb8d5d8fa62ac1d5ccf9
steven <steven.0198@hotmail.com>
2018-02-28 14:34:18 -0500

    Archivos modificados y movidos a directorio principal

--- punto1parcinume.py
+++ punto1parcinume.py
@@ -1,52 +1,29 @@
+def medidadetiempo(matriz, tama):
+	ptime = time.process_time()
+	matri(matriz, tama)
+	etime = time.process_time( )
+	return float(etime - ptime)
+
+
+def matri(matriz, tama):
+	for i in range (tama):
+		for j in range (tama):
+			print("ingrese los valores de la matriz")
+			matriz[i][j] = input()
+	for i in range (tama):
+		for j in range (tama):
+			print(matriz[i][j])
+	print("\n")
+	suma=0
+	for i in range (tama):
+		for j in range(tama):
+			if(j>i):
+				suma+=matriz[i][j]
+		print( "La suma de los elementos de la submatriz triangular superior es : ", suma)
+
+
+
 print ("digite el tamaño de las filas y las columnas de la matriz")
 tama=input()
-suma=0
-ptime = time.process_time()
-for i in range (tama):
-	for j in range (tama):
-		print("ingrese los valores de la matriz")
-		matriz[i][j] = input()
-for i in range (tama):
-	for j in range (tama):
-		print(matriz[i][j])
-print("\n")
-
-for i in range (tama):
-	for j in range(tama):
-		if(j>i):
-			suma+=matriz[i][j]
-print( "La suma de los elementos de la submatriz triangular superior es : ", suma)
-etime = time.process_time( )
-return float(etime - ptime)
-
-
-#include <stdio.h>
-#include <stdlib.h> 
-#int main(void){
-#int tama,valor;
-#printf("digite el tamaño de las filas y las columnas de la matriz");
-#scanf("%d",&tama);
-#int matriz[tama][tama];
-#printf("ingrese los valores de la matriz");
-#	for(int i=0;i<tama;i++){
-#		for(int j=0;j<tama;j++){
-#			matriz[i][j]=scanf("%d", &valor);
-#		}
-#	}
-
-#for(int i=0;i<tama;i++){
-#	for(int j=0;j<tama;j++){
-#		printf(matriz[i][j]);
-#	}
-#}
-
-#return EXIT_SUCCESS;
-#}
-
-
-
-
-
-
-
+medidadetiempo(matriz,tama)
 
